52025dad3c2edd91406d56c444589e5956df7a54
Juan <ricaudjuan@hotmail.com>
2024-08-12 19:41:56 +1000

    Edited how completing a task works

--- src/reducer/tasksReducer.js
+++ src/reducer/tasksReducer.js
@@ -1,21 +1,31 @@
 import { TASK_ACTIONS } from "./actionConstats"
 
 export const initialTasks = [
-  {
-    name: "test"
-  }
+  
 ]
 
 export function taskReducer (state, action) {
   
   switch(action.type){
     case(TASK_ACTIONS.COMPLETE_TASK): {
+      // const newTaskList = state.filter((task) => task.name !== action.payload)
+      const tasks = [...state];
+      for (let i = 0; i <= tasks.length; i++) {
+        if (tasks[i].name === action.payload) {
+          tasks[i].completed = true
+          console.log(state)
+          return tasks
+        }
+      }
+      break
+    }
+
+    case(TASK_ACTIONS.ADD_TASK): {
+      return [...state, action.payload]
+    }
+    case(TASK_ACTIONS.DELETE_TASK): {
       const newTaskList = state.filter((task) => task.name !== action.payload)
       return newTaskList
-    }
-    case(TASK_ACTIONS.ADD_TASK): {
-      console.log('entre con ' + action)
-      return [...state, action.payload]
     }
   }
 }
