ae404f502ba7f53121e71bd2def54df3e4c85b07
Juan <ricaudjuan@hotmail.com>
2024-08-12 19:42:33 +1000

    added delete task and change behavior on completed tasks

--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -3,7 +3,7 @@ import styles from "./Task.module.css"
 import { TasksContext } from "../../context/TaskContext"
 
 export function Task() {
-    const { tasks, completeTask } = useContext(TasksContext)
+    const { tasks, completeTask, deleteTask } = useContext(TasksContext)
 
     const handleClick = (description) => {
         alert(description)
@@ -17,9 +17,13 @@ export function Task() {
                         
                         <div className={styles.task_description} key={task.name}>
                             <div className={styles.task_container}>
-                                <button onClick={() => completeTask(task.name)}>Complete Task</button>
+                                {
+                                    task.completed === false ? <button onClick={() => completeTask(task.name)} type="button">Complete Task</button>
+                                    : <button>Completed</button>
+                                }
                                 <p>{task.name}</p>
                              </div>
+                             <button onClick={()=>{deleteTask(task.name)}}>Delete</button>
                             <button onClick={() => handleClick(task.description)}>Info...</button>
                         </div>
                         
